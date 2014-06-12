3fd1dbc3358e0bf67de7e19b1c9fb4ad323cc005
Michael M <mihael.muhayluk@gmail.com>
2014-06-12 15:21:37 +0300

    animation + timer

--- gen/com/svtask1/R.java
+++ gen/com/svtask1/R.java
@@ -15,6 +15,7 @@ public final class R {
         public static final int abc_slide_in_top=0x7f040003;
         public static final int abc_slide_out_bottom=0x7f040004;
         public static final int abc_slide_out_top=0x7f040005;
+        public static final int leftright=0x7f040006;
     }
     public static final class attr {
         /**  Custom divider drawable to use for elements in the action bar. 
@@ -1095,8 +1096,8 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button1=0x7f05003e;
-        public static final int button2=0x7f05003f;
+        public static final int buttonPushMe=0x7f05003e;
+        public static final int buttonReload=0x7f05003f;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1139,7 +1140,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003d;
+        public static final int textViewMessage=0x7f05003d;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
--- src/com/svtask1/MainActivity.java
+++ src/com/svtask1/MainActivity.java
@@ -1,18 +1,22 @@
 package com.svtask1;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends ActionBarActivity {			
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,7 +25,7 @@ public class MainActivity extends ActionBarActivity {
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		}			
 	}
 
 	@Override
@@ -47,18 +51,110 @@ public class MainActivity extends ActionBarActivity {
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
-	public static class PlaceholderFragment extends Fragment {
-
+	public static class PlaceholderFragment extends Fragment implements OnClickListener{		
+		
+		private Button reloadButton;
+		private Button pushMeButton;
+		private TextView messageView;
+		private CountDownTimer timer;
+		private int pressedCounter;
+		private Boolean timerStatus = false;
+		private Animation anim = null;			
+		
 		public PlaceholderFragment() {
 		}
 
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
+			View rootView = inflater.inflate(R.layout.fragment_main, container,	false);
+			
+			reloadButton = (Button)rootView.findViewById(R.id.buttonReload);
+			pushMeButton = (Button)rootView.findViewById(R.id.buttonPushMe);
+			messageView = (TextView)rootView.findViewById(R.id.textViewMessage);					
+			
+			reloadButton.setOnClickListener(this);
+			pushMeButton.setOnClickListener(this);
+			reloadButton.setVisibility(View.GONE);
+			timer = new CountDownTimer(Constants.TIMER_IN_FUTURE, Constants.TIMER_INTERVAL) {
+				
+				@Override
+				public void onTick(long millisUntilFinished) {					
+					switch ((int)millisUntilFinished / Constants.ONE_SECOND) {
+					case Constants.PUSH_30_SECOND_TO_END:
+						if(pressedCounter == Constants.ONE_PRESS)
+							reload();
+						break;
+					case Constants.PUSH_10_SECOND_TO_END : 
+						reload(); 
+						break;
+					default:
+						break;
+					}
+				}
+				
+				@Override
+				public void onFinish() {
+					reload();					
+				}
+			};
+			
 			return rootView;
 		}
-	}
+
+		@Override
+		public void onClick(View v) {			
+			switch (v.getId()) {
+			case R.id.buttonPushMe : {
+				if(!timerStatus) {
+					timer.start();
+					timerStatus = true;					
+				}
+				pressedCounter ++;
+				checkPreseedCounter();
+				break;
+			}
+			case R.id.buttonReload : 
+				reload();				
+				break;
+			default:
+				break;
+			}
+		}
+		
+		private void checkPreseedCounter(){
+			switch (pressedCounter) {
+			case Constants.ONE_PRESS: messageView.setText(getText(R.string.label_one));				
+				break;
+			case Constants.TWO_PRESSES: messageView.setText(getText(R.string.label_two));				
+				break;
+			case Constants.THREE_PRESSES: messageView.setText(getText(R.string.label_three));				
+				break;
+			case Constants.SIXT_PRESSES: messageView.setText(getText(R.string.label_sixt));				
+				break;
+			case Constants.TEN_PRESSES: messageView.setText(getText(R.string.label_ten));				
+				break;
+			case Constants.TWENTY_PRESSES: { 
+				messageView.setText(getText(R.string.label_twenty));				
+				anim = (Animation)AnimationUtils.loadAnimation(getActivity(), R.anim.leftright);
+				reloadButton.startAnimation(anim);
+				reloadButton.setVisibility(View.VISIBLE);				
+				pushMeButton.setEnabled(false);	
+				break;
+			}
+			default:
+				break;
+			}
+		}
+		
+		private void reload() {				
+			pressedCounter = 0;
+			timerStatus = false;
+			timer.cancel();			
+			pushMeButton.setEnabled(true);
+			reloadButton.setVisibility(View.GONE);
+			messageView.setText(R.string.hello_world);
+		}				
+	}	
 
 }
